9f825355d5e0f3027e49b970d8bcbf55cc11a762
vanya <vanya.moo@gmail.com>
2024-09-09 13:48:04 +0100

    Initial Commit

---
+++ Sept24A2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sept24A2
+//
+//  Created by Vanya Mutafchieva on 09/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sept24A2/Sept24A2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sept24A2App.swift
+//  Sept24A2
+//
+//  Created by Vanya Mutafchieva on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sept24A2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
